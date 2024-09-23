8d8cd252a44fc7938ae6312df910a6f61b454d90
Azhwake <azhwaq2002@gmail.com>
2024-09-23 19:58:18 +0300

    the GAME

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //code
 // gets a random number from 1 - 20
 let randomNumber = Math.floor(Math.random() * 20) + 1
-let score = 20
-let highScore = 0
+console.log(Math.trunc(Math.random() * 20) + 1)
+// let score = 20
+// let highScore = 0
